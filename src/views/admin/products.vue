993e21295a004820af4de428055398de6dba494f
nattanunkorn bunsawat <62130500025@mail.kmutt.ac.th>
2021-05-08 04:31:26 +0700

    try to get api

--- src/views/admin/products.vue
+++ src/views/admin/products.vue
@@ -1,121 +1,104 @@
 <template>
-   
-   <section class="grid h-auto grid-cols-3 px-4 mx-auto mt-16 ml-12 product max-w-auto justify-items-center lg:flex-wrap">
+  <section
+    class="grid h-auto grid-cols-3 px-4 mx-auto mt-16 ml-12 product max-w-auto justify-items-center lg:flex-wrap"
+  >
+ 
+    <div
+       v-for="(product,index) in products"
+      :key="product.name"
+      class="flex-wrap items-center p-5 px-2 mx-auto my-4 overflow-hidden bg-white rounded shadow-lg gallery "
+    >
+      <div class="rounded shadow-xl ">
+        <!--  picture -->
+        <div>
+          <button @click="callProduct(index)">
+            <!-- v-bind:src="" -->
+            <img
+              class=" h-product w-product justify-items-start hover:text-blueGrey-800 focus:outline-none"
+            />
+          </button>
+          <div id="name" class="text-xl font-light text-left">{{ products.name }}</div>
+          <div id="brand" class="text-sm font-light text-left">{{ products.brand}}</div>
+          <!-- <div class="text-sm font-light text-left">{{ products.price }}</div>
+          <div class="text-sm font-light text-left text-blueGray-400">
+            {{ products.releaseDate }}
+          </div> -->
+        </div>
 
-   
-
-     
-      <div
-        v-for="(task, index) in tasks"
-        :key="task.title"
-        class="flex-wrap items-center p-5 px-2 mx-auto my-6 my-auto overflow-hidden bg-white rounded shadow-lg gallery "
-      >
-        <div class="rounded shadow-xl ">
-          <!--  picture -->
-          <div>
-            <router-link to="#"
-              v-on:click="callProduct(index)"
-              class="d"
-            >
-              <img v-bind:src="task.image" class=" h-product w-product justify-items-start hover:text-blueGrey-800 focus:outline-none" />
-             </router-link>
-              <div class="text-xl font-light text-left" >{{task.name}}</div>
-              <div class="text-sm font-light text-left">{{task.brand}}</div>
-              <div class="text-sm font-light text-left">{{task.types}}</div>
-              <div class="text-sm font-light text-left">{{task.price}}</div>
-              <div class="text-sm font-light text-left text-blueGray-400">{{task.releaseDateate}}</div>
-            
-          </div>
-
-          <!-- click like -->
-          <div class="flex p-2 w-product" >
-            <div class="w-1 h-1 rounded-full bg-blueGray-800 ml-smallColor"></div>
-            <div class="w-1 h-1 rounded-full ml-01 bg-blueGray-800"></div>
-             <div class="w-1 h-1 rounded-full ml-01 bg-blueGray-800"></div>
-              <div class="w-1 h-1 rounded-full ml-01 bg-blueGray-800"></div>
-          </div>
+        <!-- click like -->
+        <div class="flex p-2 w-product">
+          <div class="w-1 h-1 rounded-full bg-blueGray-800 ml-smallColor"></div>
+          <div class="w-1 h-1 rounded-full ml-01 bg-blueGray-800"></div>
+          <div class="w-1 h-1 rounded-full ml-01 bg-blueGray-800"></div>
+          <div class="w-1 h-1 rounded-full ml-01 bg-blueGray-800"></div>
         </div>
       </div>
+    </div>
     <!-- </div> -->
+    <!-- </div> -->
+  </section>
   <!-- </div> -->
-
-
-    </section>
-    <!-- </div> -->
-  
-  
 </template>
 
 <script>
 // import CardSettings from "@/components/Cards/CardSettings.vue";
-import axios from 'axios';
-
+import axios from "axios";
+// axios.load;
 export default {
-
+  created() {
+    this.getProducts();
+  },
   // },
   data() {
     return {
-      tasks: [
-  
-        {
-          id: null,
-          name: "",
-          type: "",
-          image:
-            "",
-          brand:"",
-          price: null,
-          releaseDate:""
-        },
-        
-      ],
-      
       products: [],
     };
   },
-  medthod:{
-      callProduct(){
-         try{
-        const res = axios.get("http://localhost:5000/products");
-        console.log(res.data)
-      }catch{
-        console.log("err")
-      }
-      
-      }
-  }}
+  methods: {
+    getProducts() {
+      axios.get("http://localhost:5000/products")
+      .then((res) => res.data)
+      .then(data => data.forEach(
+        item => {
+           this.products = item
+          //  this.test
+           
+         }
+         ))
+     
+    },
+    // test(){
+    //   console.log(this.products.name)
+    
+    // }
+  },
+}
 </script>
 <style scoped>
 @media screen and (max-width: 1024px) {
-
-.product{
-  /* margin : 5rem; */
-  margin-left: 18rem;
-  grid-template-columns: repeat(2, minmax(0, 1fr));
-}
-.gallery{
-  margin:4rem;
-}
+  .product {
+    /* margin : 5rem; */
+    margin-left: 18rem;
+    grid-template-columns: repeat(2, minmax(0, 1fr));
+  }
+  .gallery {
+    margin: 4rem;
+  }
 }
 @media screen and (max-width: 768px) {
-
-.product{
-  margin-left: 1rem;
-  grid-template-columns: repeat(2, minmax(0, 1fr));
-  
-}
-.gallery{
-  margin:4rem;
-}
-
+  .product {
+    margin-left: 1rem;
+    grid-template-columns: repeat(2, minmax(0, 1fr));
+  }
+  .gallery {
+    margin: 4rem;
+  }
 }
 @media screen and (max-width: 480px) {
-
-.product{
-  margin : 5rem;
-  /* height: auto; */
-  grid-template-columns: repeat(1, minmax(0, 1fr));
-}
-
+  .product {
+    margin: 5rem;
+    /* height: auto; */
+    grid-template-columns: repeat(1, minmax(0, 1fr));
+  }
 }
 </style>
